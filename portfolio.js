bf29865c11da3ca56e813bdd504dbe0807a184a0
suborn <whatwevedecided@gmail.com>
2024-01-13 10:49:57 +1100

    fourth commit

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+const headerlist = document.getElementById('headerlist');
+const aboutme = document.getElementById('aboutme');
+
+headerlist.onclick = function() {
+    headerlist.style.color = black;
+};
+
+aboutmee.onclick=function(){
+    document.getElementsByClassName('skills').style.color;
+
+    
+}
